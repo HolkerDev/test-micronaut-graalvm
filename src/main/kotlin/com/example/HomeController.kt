7471139e03e9f08baddc3396bb6f84534a5929ae
Mykola Penkovskyi <36474981+HolkerDev@users.noreply.github.com>
2022-08-01 12:33:17 +0200

    add some json response

--- src/main/kotlin/com/example/HomeController.kt
+++ src/main/kotlin/com/example/HomeController.kt
@@ -1,11 +1,15 @@
 package com.example
 
 import com.example.services.HomeService
+import io.micronaut.core.annotation.Introspected
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import jakarta.inject.Inject
 import org.slf4j.LoggerFactory
+
+@Introspected
+data class Message(val message: String)
 
 @Controller
 open class HomeController(@Inject private val homeService: HomeService) {
@@ -16,5 +20,11 @@ open class HomeController(@Inject private val homeService: HomeService) {
     fun get(): HttpResponse<*> {
         logger.info("Yep! I'm actually logging stuff despite all this graalvm limitations!")
         return HttpResponse.badRequest(mapOf("message" to "Hello world! ${homeService.giveFive()}"))
+    }
+
+    @Get("/test")
+    fun getTest(): HttpResponse<*> {
+        logger.info("Yep! I'm actually logging stuff despite all this graalvm limitations!")
+        return HttpResponse.badRequest(Message("Test message to show in json response!"))
     }
 }
